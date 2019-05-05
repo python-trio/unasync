cea4fd097e0d3dc8299d8ed376f86a9e8b920957
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2019-05-05 22:20:56 +0530

    Add string formatting for better coverage

--- tests/sync/simple.py
+++ tests/sync/simple.py
@@ -1,2 +1,16 @@
+class TestImplementation:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_a_b(self):
+        s = "a is %s b is %s" % \
+            (self.a,
+            self.b)
+        return s
+
+    def f(self):
+        return 1
+
 def f():
     return 1
