76130270a92f963ee7418834d75620ad925ddd4f
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2019-05-08 19:43:29 +0530

    Add python 3.4 as py req

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     package_dir={"": "src"},
     install_requires=[],
     keywords=["async"],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "License :: OSI Approved :: Apache Software License",
