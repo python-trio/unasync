0d06a00b1170efea6e444d0f52e2b6ae8d50a414
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2019-06-15 00:47:38 +0530

    For debugging

--- tests/test_unasync.py
+++ tests/test_unasync.py
@@ -42,6 +42,7 @@ def test_unasync(tmpdir, source_file):
         unasynced_code = f.read()
         assert unasynced_code == truth
 
+
 def test_build_py_modules(tmpdir):
 
     source_modules_dir = os.path.join(TEST_DIR, "example_mod")
@@ -53,9 +54,15 @@ def test_build_py_modules(tmpdir):
     subprocess.check_call(["python", "setup.py", "build"], cwd=mod_dir, env=env)
 
     unasynced = os.path.join(mod_dir, "build/lib/_sync/some_file.py")
+    tree_build_dir = list_files("build")
+    print("unasynched --->> {}".format(unasync))
+    print("tree of build directory --->> {}".format())
+
+    assert unasynced == tree_build_dir
     with open(unasynced) as f:
         unasynced_code = f.read()
         assert unasynced_code == "def f():\n    return 1\n"
+
 
 def test_build_py_packages(tmpdir):
 
@@ -67,7 +74,9 @@ def test_build_py_packages(tmpdir):
     env["PYTHONPATH"] = os.path.realpath(os.path.join(TEST_DIR, ".."))
     subprocess.check_call(["python", "setup.py", "build"], cwd=pkg_dir, env=env)
 
-    unasynced = os.path.join(pkg_dir, "build", "lib", "example_pkg", "_sync", "__init__.py")
+    unasynced = os.path.join(
+        pkg_dir, "build", "lib", "example_pkg", "_sync", "__init__.py"
+    )
     with open(unasynced) as f:
         unasynced_code = f.read()
         assert unasynced_code == "def f():\n    return 1\n"
