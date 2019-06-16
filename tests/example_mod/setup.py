899ff03826884e9799318b4f08ed9d142b454dcf
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2019-06-16 19:51:17 +0530

    update py_module in example mod

--- tests/example_mod/setup.py
+++ tests/example_mod/setup.py
@@ -9,6 +9,6 @@ setuptools.setup(
     author_email="author@example.com",
     description="A package used to test unasync",
     url="https://github.com/pypa/sampleproject",
-    py_modules=["_async/some_file",],
+    py_modules=["_async.some_file",],
     cmdclass={"build_py": unasync.build_py},
 )
