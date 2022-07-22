69cfc3ce103569088dfd8137f235e4199f1f406c
Quentin Pradet <quentin.pradet@gmail.com>
2022-07-22 12:47:32 +0200

    Replace tokenization with tokenize_rt
    The standard library module `tokenize` does not round trip, so we had to
    implement our own tokenization on top of it. However, tokenize_rt does
    it better, so let's adopt it instead.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     include_package_data=True,
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=[],
+    install_requires=["tokenize_rt==4.2.1"],
     keywords=["async"],
     python_requires=">=3.7",
     classifiers=[
