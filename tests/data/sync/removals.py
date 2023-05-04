aaae7db7a30c7d9f09ab754c01bc3c619c0819c6
Spencer Phillip Young <spencer.young@spyoung.com>
2023-05-03 17:57:22 -0700

    Add 'unasync: remove' feature

---
+++ tests/data/sync/removals.py
@@ -0,0 +1,23 @@
+from common import (
+a, b , c  # these should stick around
+)
+
+# these imports should be removed
+
+CONST = 'foo'
+
+def foo():
+    print('this function should stick around')
+
+
+
+
+
+class Foo:
+    def foobar(self):
+        print('This method should stick around')
+
+
+    def another_method(self):
+        print('This line should stick around')
+
